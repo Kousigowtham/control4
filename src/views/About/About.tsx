ea6a99989d19cf3f8b8e0f8fbd2bd093c7b97f78
KOUSIK K A <kowsik.a@techjays.com>
2023-03-14 14:49:11 +0530

    ADD: login page and about page

---
+++ src/views/About/About.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./About.module.css";
+
+const About = () => {
+  return <div className={styles.container}>About Page</div>;
+};
+
+export default About;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate("/about");
+  };
+
+  React.useEffect(() => {
+    window.navigateToAbout = () => {
+      navigate("/about");
+    };
+  }, []);
+  return (
+    <main className={styles.container}>
+      <h3>Welcome to Techjays-Control4 test page</h3>
+      <form onSubmit={submitHandler} id="login-form">
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit" id="login-btn">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default Home;
